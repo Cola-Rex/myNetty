c03255b751d0d72a1bd1f1266f8207f18abd54da
Ezreal <1649811141@qq.com>
2018-10-05 20:24:03 +0800

    netty服务端的实现

---
+++ src/main/java/simple/server/ServerHandler.java
@@ -0,0 +1,21 @@
+package simple.server;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) {
+		System.out.println("server channelRead..");
+		System.out.println(ctx.channel().remoteAddress() + "->Server" + msg.toString());
+		ctx.write("server write" + msg);
+		ctx.flush();
+	}
+	
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		cause.printStackTrace();
+		ctx.close();
+	}
+}
